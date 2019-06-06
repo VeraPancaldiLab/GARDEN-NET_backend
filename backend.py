1d2c7ff62e1dd1cb9a1a14d03053dd52c7266038
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-06-06 11:04:55 +0200

    backend.py: Use redis container link address instead localhost

--- backend.py
+++ backend.py
@@ -19,8 +19,8 @@ ALLOWED_EXTENSIONS = set(["bed.gz"])
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 # Add redis broker to Flask app
-app.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
-app.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/0"
+app.config["CELERY_BROKER_URL"] = "redis://redis:6379/0"
+app.config["CELERY_RESULT_BACKEND"] = "redis://redis:6379/0"
 # Initialize celery distributed task queue
 celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
 celery.conf.update(app.config)
