9a38f4b09a7f7bd7f03ac334aa6fae9dc78b1e25
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-07-16 18:41:17 +0200

    pipeline.sh: Don't generate chromosomes with flag metadata only

--- pipeline.sh
+++ pipeline.sh
@@ -85,7 +85,7 @@ for file in $(realpath "$input"/*); do
       rmdir "$output_folder/$organism/$cell_type/chromosomes" 2>/dev/null
       # $chromosomes_seq_string has to be really splited by spaces in words so disable linter here
       # shellcheck disable=SC2086
-      parallel --eta ./network_generator.R "--PCHiC $file $features_parameter --chromosome {} --pipeline $output_folder $intronic_parameter --alias ./alias_databases/${organism}.tsv ${bait_names_parameter} | sed -e '/chr/! s/\"[[:space:]]*\([[:digit:]]*\.\?[[:digit:]]\+\)\"/\1/' > /dev/null" ::: $chromosomes_seq_string
+      parallel --eta ./network_generator.R "--PCHiC $file $features_parameter --chromosome {} --pipeline $output_folder $intronic_parameter --alias ./alias_databases/${organism}.tsv ${bait_names_parameter} | sed -e '/chr/! s/\"[[:space:]]*\([[:digit:]]*\.\?[[:digit:]]\+\)\"/\1/' > /dev/null" ::: 1
     fi
     ;;
   esac
