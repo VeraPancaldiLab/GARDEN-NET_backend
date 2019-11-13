fb0fd16b6c02fd1c99ea179079318e4cb5b53a73
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-11-13 13:53:42 +0100

    chromosomes_positions_checker.sh: Surround variable between double quotes

--- chromosomes_positions_checker.sh
+++ chromosomes_positions_checker.sh
@@ -6,17 +6,17 @@ fi
 
 # Let globbing to be recursive
 shopt -s globstar
-for chromosome_file in "$(realpath $1)"/**/chromosomes/chr*.json; do
+for chromosome_file in "$(realpath "$1")"/**/chromosomes/chr*.json; do
   case $chromosome_file in
-    *)
-      x=$(jq '.nodes[0].position.x' < "$chromosome_file")
-      y=$(jq '.nodes[0].position.y' < "$chromosome_file")
-      bad_positions=$(bc 2> /dev/null <<< "$x == $y && $x == 0")
-      if [ "$bad_positions" == "" ]; then\
-        echo -e "$chromosome_file: \e[31m\e[1mis empty!\e[0m"
-      elif [ "$bad_positions" -eq 1 ]; then
-        echo -e "$chromosome_file: \e[31m\e[1mnot generated positions!\e[0m"
-      fi
-      ;;
+  *)
+    x=$(jq '.nodes[0].position.x' <"$chromosome_file")
+    y=$(jq '.nodes[0].position.y' <"$chromosome_file")
+    bad_positions=$(bc 2>/dev/null <<<"$x == $y && $x == 0")
+    if [ "$bad_positions" == "" ]; then
+      echo -e "$chromosome_file: \e[31m\e[1mis empty!\e[0m"
+    elif [ "$bad_positions" -eq 1 ]; then
+      echo -e "$chromosome_file: \e[31m\e[1mnot generated positions!\e[0m"
+    fi
+    ;;
   esac
 done
