e42a368e1f85c9ebdd48a7b79a9ec6226ec73b7e
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-12-13 17:42:51 +0100

    chromosomes_positions_checker.sh: Check exponential numbers in chromosome positions

--- chromosomes_positions_checker.sh
+++ chromosomes_positions_checker.sh
@@ -12,10 +12,15 @@ for chromosome_file in "$(realpath "$1")"/**/chromosomes/chr*.json; do
     x=$(jq '.nodes[0].position.x' <"$chromosome_file")
     y=$(jq '.nodes[0].position.y' <"$chromosome_file")
     bad_positions=$(bc 2>/dev/null <<<"$x == $y && $x == 0")
+    start=$(jq '.nodes[0].data.start' <"$chromosome_file")
+    end=$(jq '.nodes[0].data.end' <"$chromosome_file")
+    bad_chr_positions=$(bc 2>/dev/null <<<"$start == $end && $start == 1")
     if [ "$bad_positions" == "" ]; then
       echo -e "$chromosome_file: \e[31m\e[1mis empty!\e[0m"
     elif [ "$bad_positions" -eq 1 ]; then
       echo -e "$chromosome_file: \e[31m\e[1mnot generated positions!\e[0m"
+    elif [[ "$bad_positions" == "" || "$bad_chr_positions" -eq 1 ]]; then
+      echo -e "$chromosome_file: \e[31m\e[1mexponential number as chromosome positions!\e[0m"
     fi
     ;;
   esac
